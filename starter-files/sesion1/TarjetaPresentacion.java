34fb23c67d9c0f8b819e217fd653a752139737e5
PengwinKingdom <kai.supernova097@gmail.com>
2025-11-14 07:43:37 -0500

    Update TarjetaPresentacion.java

--- starter-files/sesion1/TarjetaPresentacion.java
+++ starter-files/sesion1/TarjetaPresentacion.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // TODO: Escribe aquí tu presentación
+        // TODO: escribe tu título
+        // TODO: agrega 3 datos sobre ti
     }
 }
