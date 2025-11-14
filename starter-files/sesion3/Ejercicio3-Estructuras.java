233566d97fa90e74bc3493614778a8d7159c5caf
moisespinoza <149260312+MoisesECh@users.noreply.github.com>
2025-11-14 17:26:17 -0500

    docs: add ejercicio 3

---
+++ starter-files/sesion3/Ejercicio3-Estructuras.java
@@ -0,0 +1,18 @@
+// ESTRUCTURAS DE DECISION
+
+public class Main {
+    public static void main(String[] args) {
+     
+      int nota=19;
+       // TODO: Añade las condiciones para que la nota sea aprobatoria o desaprobatoria
+      
+      if (nota >= 18) {
+            System.out.println("Excelente");
+      } 
+      //TODO: agrega las demás condiciones para que la nota sea muy buena, buena o necesite mejorar
+       
+      int opcion = 1;
+      // TODO: Añade las condiciones para cada case del switch, y agrega el default (no te olvides) 
+        
+    }
+}
