a96ad9d3c2bc054ce5cdbed3470d14fc73f43973
PengwinKingdom <kai.supernova097@gmail.com>
2025-11-14 05:11:14 -0500

    Update and rename a to mini-reto-solution.java

---
+++ completed-examples/sesion1/mini-reto-solution.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("===== Mi Presentación =====");
+        System.out.println("Nombre:\tTuNombre");
+        System.out.println("Hobby:\tTuHobby");
+        System.out.println("Meta:\tTuMeta");
+        System.out.println("Frase:\t\"Programar es pensar en pasos\"");
+    }
+}
+
