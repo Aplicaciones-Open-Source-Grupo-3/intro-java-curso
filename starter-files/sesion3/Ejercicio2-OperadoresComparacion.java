3458b9eb7debf8facc714efe1a25bae11fb0b1da
moisespinoza <149260312+MoisesECh@users.noreply.github.com>
2025-11-14 17:19:46 -0500

    docs: add ejercicio 2

---
+++ starter-files/sesion3/Ejercicio2-OperadoresComparacion.java
@@ -0,0 +1,8 @@
+// OPERADORES DE COMPARACION Y LOGICOS
+public class Main {
+    public static void main(String[] args) {
+      //TODO: declara la variable
+      //TODO: crea la condicion booleana
+      //TODO: imprime el resultado
+    }
+}
