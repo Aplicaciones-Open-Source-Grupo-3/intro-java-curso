0ba8e4d422d0de76201c6f92cad46f53cb265aca
moisespinoza <149260312+MoisesECh@users.noreply.github.com>
2025-11-14 16:59:28 -0500

    docs: add example 2

---
+++ completed-examples/sesion3/ejercicio2-solution.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+       int edad= 15;
+       
+       boolean esMayor=edad>=18;
+        System.out.println("Eres mayor de edad?" + esMayor);
+    }
+}
