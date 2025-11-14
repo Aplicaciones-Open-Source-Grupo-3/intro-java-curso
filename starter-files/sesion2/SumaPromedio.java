03150afb4edc24722fbaafed393c62bda23f42f0
PengwinKingdom <kai.supernova097@gmail.com>
2025-11-14 07:36:53 -0500

    Update and rename a to SumaPromedio.java

---
+++ starter-files/sesion2/SumaPromedio.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // TODO: pedir dos números al usuario
+        // TODO: calcular suma, resta, multiplicación y división
+        // TODO: mostrar los resultados
+
+        sc.close();
+    }
+}
+
