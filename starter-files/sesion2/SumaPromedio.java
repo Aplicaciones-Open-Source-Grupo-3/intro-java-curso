d371252cedff68847fef469a76f8fb2533a357eb
PengwinKingdom <kai.supernova097@gmail.com>
2025-11-14 07:37:23 -0500

    Update SumaPromedio.java

--- starter-files/sesion2/SumaPromedio.java
+++ starter-files/sesion2/SumaPromedio.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // TODO: pedir dos números al usuario
+        // TODO: pedir tres números al usuario
         // TODO: calcular suma, resta, multiplicación y división
         // TODO: mostrar los resultados
 
