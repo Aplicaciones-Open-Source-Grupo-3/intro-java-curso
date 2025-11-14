ac23bce7f1acada1d27a9de9980d7f3b8e277995
PengwinKingdom <kai.supernova097@gmail.com>
2025-11-14 07:00:17 -0500

    Add Java solution for ejercicio2

---
+++ completed-examples/sesion1/ejercicio2-solution.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("===== Mi Tarjeta =====");
+        System.out.println("Curso:\t\tJava");
+        System.out.println("Mensaje:\tProgramar es divertido");
+        System.out.println("======================");
+    }
+}
